61bea031a337c831cb08ae9ebfe1cedbdec44143
Bhargav <kuchi.bhargav@gmail.com>
2017-06-02 16:52:10 -0500

    Update PowerSet.java

--- PowerSet.java
+++ PowerSet.java
@@ -26,4 +26,25 @@ public class PowerSet {
             }            
            System.out.println(result);
         }
+    
+    //another version for only int datatype
+    private static void printPowerset(int n) {
+        int stack[] = new int[n + 1], k;
+        stack[0] = 0;
+        k = 0;
+        while (true) {
+            System.out.println();
+            if (stack[k] < n) {
+                stack[k + 1] = stack[k] + 1;
+                k++;
+            } else {
+                stack[k - 1]++;
+                k--;
+            }
+            if (k == 0)
+                break;
+            for (int i = 1; i <= k; i++)
+                System.out.print(stack[i] + " ");
+        }
+    }    
 }
